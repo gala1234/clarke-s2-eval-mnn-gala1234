63f828816f341cac0214e6e7a29134db0e36bd36
gala1234 <calero.vg@gmail.com>
2017-12-10 00:57:58 +0100

    write some JS

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict'
+
+
+	//Número aleatorio
+	var randomNumber = Math.floor((Math.random() * 100) + 1);
+	//Localizo en DOM la lista de ganadores
+	var winnersRanking = document.querySelector('score-zone-ul');
+
+
+	//Empieza el juego
+
+document.querySelector('play-button').onclick = function (){
+	var guess = document.getElementById('play-input').value;
+	checkGuess(guess);
+	counter++;
+	}
+
+function checkGuess(guess){
+	if (guess == randomNumber){
+		document.querySelector('instructions-p').innerHTML = 'ACERTADO';
+		document.querySelector('user-data-name').style.display = 'flex';
+		document.querySelector("play-button").disabled = true;
+	} else if (guess > randomNumber){
+		alert("Too high!");
+		document.querySelector("guess").value='';
+	} else (guess < randomNumber){
+		alert("Too low!");
+		document.querySelector("guess").value='';
+	}
+}
