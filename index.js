2fd11bbeecbdb3c5b4643cf5cf6f1a1be575d7bd
gala1234 <calero.vg@gmail.com>
2017-12-12 13:34:23 +0100

    solved ranking isues

--- index.js
+++ index.js
@@ -1,49 +1,68 @@
 'use strict'
 
 
-	//Número aleatorio
-	var randomNumber = Math.floor((Math.random() * 100) + 1);
-	//Localizo numero que mete el jugador
-	var guess = document.querySelector('.play-input');
-	//Boton de jugar
-	var button = document.querySelector('.play-button'), counter = 0;
-	//Localizo en DOM la lista de ganadores
-	var winnersRanking = document.querySelector('.score-zone');
-	var winner = {};
-	winner.name = 'user-data-name';
-	winner.attempts = 'counter';
-	var winnersList = '<li>winner.name - winner.attempts</li>';
-	var counter =
-	//Empieza el juego
-
-function checkGuess(guess){
-	if (guess == randomNumber){
-		document.querySelector('.instructions-p').innerHTML = 'ACERTADO';
-		document.querySelector('.user-data-name').style.display = 'flex';
-		button.disabled = true;
-		function addWinner(){
-			winnersList.innerHTML = winnersList.innerHTML + winner;
-		}
-		function restart (){
-			counter = 0;
-			document.querySelector('.header-attempts').innerHTML = 'counter';
-			var randomNumber = Math.floor((Math.random() * 100) + 1);
-			document.querySelector('.user-data-name').style.display = 'none';
-			document.querySelector('.instructions-p').innerHTML = 'Escribe un número y dale a <em>Prueba</em>';
-			guess.innerHTML = '';
-			button.disabled = false;
-		}
-	} else if (guess > randomNumber){
-		counter++
-		document.querySelector('.instructions-p').innerHTML = 'Demasiado alto';
-		document.querySelector('.header-attempts').innerHTML = 'counter';
-		guess.innerHTML = '';
-	} else {
-		counter++
-		document.querySelector('.instructions-p').innerHTML = 'Demasiado bajo';
-		document.querySelector('.header-attempts').innerHTML = 'counter';
-		guess.innerHTML = '';
-	}
+//Número aleatorio
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+//Localizo numero que mete el jugador
+var guess = document.querySelector('.play-input');
+//Boton de jugar
+var button = document.querySelector('.play-button'), counter = 0;
+//Localizo en DOM la lista de ganadores
+var winnerName = document.querySelector('.user-data-name');
+//Localizo boton de guardar datos del ganadores
+var saveButton = document.querySelector('.user-data-button');
+//Creo el ranking
+var winnersRanking = document.querySelector('.score-zone-ul');
+var winnersList = '';
+var winner = {
+	name: [],
+	attempts: []
 }
 
-button.addEventListener('click', checkGuess(guess));
+
+alert(randomNumber);
+
+//Empieza el juego
+
+//Probar numero y comparar
+function checkGuess(){
+	counter++;
+	document.querySelector('.header-attempts').innerHTML = counter;
+	var number = parseInt(guess.value);
+	if (number===randomNumber){
+		document.querySelector('.instructions-p').innerHTML = 'ACERTADO';
+		document.querySelector('.user-data').style.display = 'flex';
+		button.disabled = true;
+	} else if (number>randomNumber){
+		document.querySelector('.instructions-p').innerHTML = 'Demasiado alto';
+		guess.value = '';
+	} else {
+		document.querySelector('.instructions-p').innerHTML = 'Demasiado bajo';
+		guess.value = '';
+	}
+}
+button.addEventListener('click', checkGuess);
+
+//Administar datos del ganador y reiniciar
+function addWinner(){
+	winner.name.push(winnerName.value);
+	winner.attempts.push(counter);
+	winnersList = '<li>' + winnerName.value + ' - ' + counter + '</li>';
+	winnersRanking.innerHTML = winnersRanking.innerHTML + winnersList;
+}
+function restart (){
+	counter = 0;
+	document.querySelector('.header-attempts').innerHTML = counter;
+	document.querySelector('.user-data').style.display = 'none';
+	document.querySelector('.instructions-p').innerHTML = 'Escribe un número y dale a <em>Prueba</em>';
+	guess.value = '';
+	button.disabled = false;
+}
+saveButton.addEventListener('click', function(){
+	addWinner();
+	restart();
+	randomNumber = Math.floor((Math.random() * 100) + 1);
+});
+
+
+//Que pruebe al pulsar Enter
